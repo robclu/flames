bbae8d22ec040eb4f4916961d9728a040be9d739
RobClu <robjclucas@gmail.com>
2020-05-22 21:11:07 +0100

    Refactor resnetv2 to handle any layer sizes

--- include/flame/models/resnet_v2.hpp
+++ include/flame/models/resnet_v2.hpp
@@ -22,8 +22,7 @@
 namespace flame::models {
 
 /// Thhis class defines an implementation of a residual neural network,
-/// version
-/// 2. It can be configured by changing the type of the block.
+/// version 2. It can be configured by changing the type of the block.
 /// \tparam Block The type of the block for the network.
 template <typename Block>
 class ResnetV2Impl : public torch::nn::Module {
@@ -40,7 +39,6 @@ class ResnetV2Impl : public torch::nn::Module {
   static constexpr int64_t first_conv_outputs = 64;
   static constexpr int64_t classes_def        = 1000;
   static constexpr int64_t in_channels_def    = 3;
-  static constexpr int64_t fc_inputs          = 2048;
   static constexpr int64_t maxpool_size       = 7;
   static constexpr int64_t avgpool_size       = 7;
 
@@ -66,12 +64,12 @@ class ResnetV2Impl : public torch::nn::Module {
     _batchnorm{first_conv_outputs},
     _relu{relu_options()},
     _max_pool{maxpool_options()},
+    _layer_1{make_layer(layer_1_out_channels, layer_sizes[0], stride_2)},
+    _layer_2{make_layer(layer_2_out_channels, layer_sizes[0], stride_2)},
+    _layer_3{make_layer(layer_3_out_channels, layer_sizes[0], stride_2)},
+    _layer_4{make_layer(layer_4_out_channels, layer_sizes[0])},
     _avg_pool{avgpool_options()},
-    _fc{fc_inputs, classes} {
-    make_layer(_layer_1, layer_1_out_channels, layer_sizes[0], stride_2);
-    make_layer(_layer_2, layer_2_out_channels, layer_sizes[1], stride_2);
-    make_layer(_layer_3, layer_3_out_channels, layer_sizes[2], stride_2);
-    make_layer(_layer_4, layer_4_out_channels, layer_sizes[3]);
+    _fc{_in_channels, classes} {
     register_module("conv", _conv);
     register_module("batchnorm", _batchnorm);
     register_module("relu", _relu);
@@ -110,17 +108,17 @@ class ResnetV2Impl : public torch::nn::Module {
  private:
   int64_t _in_channels = first_conv_outputs; //!< Number of input channels.
   int64_t _classes     = 0;                  //!< The number of classes.
-  Conv    _conv        = nullptr;            //!< First convoltional layer.
-  Norm    _batchnorm   = nullptr;            //!< Batchnorm layer.
-  Relu    _relu        = nullptr;            //!< Relu layer.
-  MaxPool _max_pool    = nullptr;            //!< Max pooling layer.
-  AvgPool _avg_pool    = nullptr;            //!< Average pool layer.
-  FeatConnector _fc    = nullptr;            //!< Feature connector.
 
-  Layer _layer_1; //!< First layer of blocks.
-  Layer _layer_2; //!< Second layer of block.
-  Layer _layer_3; //!< Third layer of blocks.
-  Layer _layer_4; //!< Fourth layer of blocks.
+  Conv          _conv      = nullptr; //!< First convoltional layer.
+  Norm          _batchnorm = nullptr; //!< Batchnorm layer.
+  Relu          _relu      = nullptr; //!< Relu layer.
+  MaxPool       _max_pool  = nullptr; //!< Max pooling layer.
+  Layer         _layer_1;             //!< First layer of blocks.
+  Layer         _layer_2;             //!< Second layer of block.
+  Layer         _layer_3;             //!< Third layer of blocks.
+  Layer         _layer_4;             //!< Fourth layer of blocks
+  AvgPool       _avg_pool = nullptr;  //!< Average pool layer.
+  FeatConnector _fc       = nullptr;  //!< Feature connector.
 
   /// Returns the defualy convolution for the first layer.
   static auto default_first_conv() -> Conv {
@@ -147,14 +145,18 @@ class ResnetV2Impl : public torch::nn::Module {
   }
 
   /// Makes a layer with \p out_channels output channels, \p blocks blocks in
-  /// the layer, and \p stride for the convolutions in the layer.
-  /// \param layer        The layer to create.
+  /// the layer, and \p stride for the convolutions in the layer. It returns the
+  /// newly created layer.
+  ///
+  /// \post The _in_channels class member is modified to represent the number of
+  ///       inputs to a layer which follows this.
+  ///
   /// \param out_channels The number of output channels.
   /// \param blocks       The number of blocks in the layer.
   /// \param stride       The stride in the layer convolutions.
-  auto make_layer(
-    Layer& layer, int64_t out_channels, int64_t blocks, int64_t stride = 1)
-    -> void {
+  auto make_layer(int64_t out_channels, int64_t blocks, int64_t stride = 1)
+    -> Layer {
+    Layer      layer;
     Layer      downsample = nullptr;
     const auto out_planes = out_channels * Block::expansion;
     if (stride != stride_1 || _in_channels != out_planes) {
@@ -168,12 +170,13 @@ class ResnetV2Impl : public torch::nn::Module {
     for (int64_t i = 1; i != blocks; ++i) {
       layer->push_back(Block(_in_channels, out_channels));
     }
+    return layer;
   }
 };
 
-/// Make the implementation into a torch module.
+/// Make the resnet implementation into a torch module.
 /// See `pimpl.h` in pytorch documentation.
-/// We can't use the macro here because of the template.
+/// The torch module macro can't be used here here because of the template.
 /// \tparam Block The type of the block for the network.
 template <typename Block>
 class ResnetV2 : public torch::nn::ModuleHolder<ResnetV2Impl<Block>> {
